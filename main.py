3f2963a24a602b6171c38c669dee280df40b9471
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-04 09:44:07 -0700

    Implement password length validation (minimum 8 characters)

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def password_strength(password):
     feedback = []
     score = 0
 
+    #Add password length check
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Password should be at least 8 characters long.")
+
 
 
 
