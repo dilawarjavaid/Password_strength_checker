294007dbcdf926fc56d6fe51990945779b25e7fe
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-04 09:42:46 -0700

    Load English dictionary words from NLTK corpus into a set

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from string import punctuation
 
 # Download the NLTK words corpus
 nltk.download('words')
+#Load dictionary words into a set for fast lookup
+dictionary_words = set(words.words())
+
+
+
+
